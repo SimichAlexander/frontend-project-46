c1ae784a70f281621d83f17df7edf9b3e8c226ba
Alexander Simich <simichalexander@gmail.com>
2024-03-03 23:12:30 +0300

    project step 2.0

---
+++ src/gendiff.js
@@ -0,0 +1,13 @@
+import { program } from 'commander';
+
+const gendiff = () => {
+  program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+
+  program.parse();
+};
+
+export default gendiff;
