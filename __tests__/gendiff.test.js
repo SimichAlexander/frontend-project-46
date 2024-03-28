1cae0c611e468a31b6b6ffa78b6118247eea758e
Alexander Simich <simichalexander@gmail.com>
2024-03-28 10:22:16 +0300

    project step 9.5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,13 +6,24 @@ import main from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 const expectedStylishFormat = fs.readFileSync(getFixturePath('expected-stylish-format.txt'), {
   encoding: 'utf8',
 });
 const expectedPlainFormat = fs.readFileSync(getFixturePath('expected-plain-format.txt'), {
   encoding: 'utf8',
+});
+const expectedJsonFormat = fs.readFileSync(getFixturePath('expected-json-format.txt'), {
+  encoding: 'utf8',
+});
+
+test('stylish-format-diff-json', () => {
+  expect(main('./file1.json', './file2.json', 'stylish')).toBe(expectedStylishFormat);
+});
+
+test('stylish-format-diff-yaml', () => {
+  expect(main('./file1.yml', './file2.yml', 'stylish')).toBe(expectedStylishFormat);
 });
 
 test('plain-format-diff-json', () => {
@@ -23,10 +34,10 @@ test('plain-format-diff-yaml', () => {
   expect(main('./file1.yml', './file2.yml', 'plain')).toBe(expectedPlainFormat);
 });
 
-test('stylish-format-diff-json', () => {
-  expect(main('./file1.json', './file2.json', 'stylish')).toBe(expectedStylishFormat);
+test('json-format-diff-json', () => {
+  expect(main('./file1.json', './file2.json', 'json')).toBe(expectedJsonFormat);
 });
 
-test('stylish-format-diff-yaml', () => {
-  expect(main('./file1.yml', './file2.yml', 'stylish')).toBe(expectedStylishFormat);
+test('json-format-diff-yaml', () => {
+  expect(main('./file1.yml', './file2.yml', 'json')).toBe(expectedJsonFormat);
 });
