508811c2363d3686ae7ced1901257c1fb0abb420
Alexander Simich <simichalexander@gmail.com>
2024-04-04 12:20:06 +0300

    fix 12

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,18 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const formatter = (object, format) => {
-  const mapping = {
-    plain: plain(object),
-    json: json(object),
-    stylish: stylish(object),
-  };
-  if (!mapping[format]) {
+const mapping = {
+  plain,
+  json,
+  stylish,
+};
+
+const formatter = (object, type) => {
+  const format = mapping[type];
+  if (!format) {
     throw new Error(`Unknown format '${format}'`);
   }
-  return mapping[format];
+  return format(object);
 };
 
 export default formatter;
